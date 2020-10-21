94c345a5e67308b96f471909cfb733f7e1def493
ilpython <hunt1574@foxmail.com>
2020-10-21 19:58:49 +0800

    update config.js
    update

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 window.Config = {
 
   // 站点名
-  SiteName: '站点状态监控---极客青年',
+  SiteName: '站点状态监控',
 
   // 站点链接
   SiteUrl: '/',
@@ -10,7 +10,7 @@ window.Config = {
   // UptimeRobot Api Keys
   // 支持 Monitor-Specific 和 Read-Only 两种 Api Key
   ApiKeys: [
-    'ur894242-90d1eb90acc82624bcaed27a'
+    'ur1059100-0e998614ca1bde7b5b1f96db'
   ],
 
   // 是否显示监测站点的链接
@@ -25,15 +25,11 @@ window.Config = {
   Navi: [
     {
       text: '项目主页',
-      url: 'https://github.com/geekyouth/uptime-status'
+      url: 'https://github.com/ilpython/uptime-status'
     },
     {
       text: 'GitHub',
       url: 'https://github.com/geekyouth/'
-    },
-    {
-      text: '博客',
-      url: 'https://java666.cn'
     }
   ]
 };
